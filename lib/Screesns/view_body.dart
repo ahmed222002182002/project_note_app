0b6c5fa25918621640496189a4fc94dd22b78f80
DELL <ahmedyassewywo@gmail.com>
2025-08-16 23:45:29 +0300

    creat App par

--- lib/Screesns/view_body.dart
+++ lib/Screesns/view_body.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:notes_app_project/custom_widget/Custom_icon.dart';
 class ViewBody extends StatelessWidget {
   const ViewBody({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Column();
+    return const Column(
+      children: [
+        Row(
+          children: [
+            SizedBox(
+              height: 180,
+            ),
+            Text('Notes',style: TextStyle(
+              fontSize: 33,
+            ),),
+            Spacer(),
+            CustomIconbutten(),
+          ],
+        )
+      ],
+    );
   }
 }
+
---
+++ lib/custom_widget/Custom_icon.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+class CustomIconbutten extends StatelessWidget {
+  const CustomIconbutten({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return   Container(
+      height: 55,
+      width: 60,
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.02),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: IconButton(onPressed: () {},icon:Icon(Icons.search),iconSize: 35,),
+    );
+  }
+}
