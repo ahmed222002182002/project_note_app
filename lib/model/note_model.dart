5538ab2c2e04a23dc68ae9879e93d522a86e92d2
DELL <ahmedyassewywo@gmail.com>
2025-09-05 14:41:52 +0300

    create note_model

---
+++ lib/model/note_model.dart
@@ -0,0 +1,12 @@
+class Notemodel{
+ final String title;
+ final String subtitel;
+ final String data;
+ final int color;
+ Notemodel({
+ required this.title,
+  required this.subtitel,
+  required this.data,
+   required this.color
+});
+}
