96243db0e8d18d288987cd7b0e82a56e2c29fdec
DELL <ahmedyassewywo@gmail.com>
2025-09-07 14:48:08 +0300

    create block Simple observed

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app_project/Screesns/first_view.dart';
+import 'package:notes_app_project/cubit/bloc_simple_observ.dart';
 import 'package:notes_app_project/cubit/create_cubit.dart';
 import 'package:notes_app_project/helper/helper.dart';
 import 'package:notes_app_project/model/note_model.dart';
 void main() async{
+  Bloc.observer=SimpleBloc();
    await Hive.initFlutter();
   Hive.registerAdapter(NotemodelAdapter());
  await Hive.openBox(kNotsBox);
